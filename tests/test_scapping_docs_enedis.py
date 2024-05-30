edc28c9ac8736ddadadba2f2a95b15aaef9502c3
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2024-05-30 12:28:26 +0200

    commit

--- tests/test_scapping_docs_enedis.py
+++ tests/test_scapping_docs_enedis.py
@@ -1,5 +1,35 @@
+from scrapping.list_docs_enedis import ScrapListDocsEnedis
+import pytest
 
-def test_get_list_doc_from_page():
+@pytest.fixture()
+def resource():
+    print("setup")
+    yield "resource"
+    print("teardown")
+
+class TestScrapListDocsEnedis:
+    def test_that_depends_on_resource(self, resource):
+        print("testing {}".format(resource))
+
+def test_load_page():
     assert True
 
-def test_always_fails():
+
+def test_get_page_data():
+    assert True
+
+
+def test_scrap_all():
+    assert True
+
+
+def test_scrap_all():
+    assert True
+
+ 
+def test_create_dataframe():
+    assert True
+
+
+def test_save_dataframe_to_csv():
+    assert True

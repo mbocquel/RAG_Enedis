4327789e1f188ca1a9d6f4a48bd960b60e9a10cd
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2024-05-31 11:29:34 +0200

    Unit test scrap passed

--- scrapping/list_docs_enedis.py
+++ scrapping/list_docs_enedis.py
@@ -12,38 +12,87 @@ class ScrapListDocsEnedis:
         self.url_visited = []
         self.url_base = 'https://www.enedis.fr/documents'
         self.curent_page = None
+        self.dataframe = None
+        self.num_page = 0
     
     
     def load_page(self):
         """Load a page and parse it, then store it in self.curent_page"""
-        pass
-
+        if self.num_page < 0:
+            return
+        url = self.url_base + "?page=" + str(self.num_page)
+        response = requests.get(url)
+        self.url_visited.append(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        self.curent_page = soup.select(".press-page__field")
+        next = soup.select(".pager__next")
+        if next:
+            self.num_page += 1
+        else:
+            self.num_page = -1
+        
 
     def get_page_data(self) -> list:
         """
         Collect the data from a page, return value : 
         [['url', 'type', "date", "content", "file_info"], [...]]
         """
-        pass
+        if not self.curent_page:
+            return
+        scrapping = []
+        scrap_elem = []
+        sep_list = [elem == self.curent_page[0] for elem in self.curent_page]
+        for i in range(1, len(sep_list)):
+            if sep_list[i]:
+                scrapping.append(scrap_elem)
+                scrap_elem = []
+            else:
+                scrap_elem.append([elem for elem in self.curent_page[i].contents if elem != '\n'])
+        scrapping.append(scrap_elem)
+
+        for pdf in scrapping:
+            new_data = [[""]] * 5
+            for elem in pdf:
+                if type(elem) == list and len(elem):
+                    if "attrs" in elem[0].__dict__ and 'href' in elem[0].__dict__.get("attrs") and 'aria-label' not in elem[0].__dict__.get("attrs"):
+                        new_data[0] = "https://www.enedis.fr" + elem[0].__dict__.get("attrs").get('href')
+                    elif "attrs" in elem[0].__dict__ and 'datetime' in elem[0].__dict__.get("attrs"):
+                        new_data[2] = elem[0].__dict__.get("attrs").get('datetime')
+                    elif "attrs" in elem[0].__dict__ and 'aria-label' in elem[0].__dict__.get("attrs"):
+                        new_data[4] = elem[0].__dict__.get("attrs").get('aria-label')
+                    elif new_data[1] == [""]:
+                        new_data[1] = str(elem[0]).strip().replace(",", ";")
+                    else:
+                        new_data[3] = str(elem[0]).strip().replace(",", ";")
+            self.data.append(new_data)
+
 
     def scrap_all(self):
         """
         Scrap all pages of the enedis document page
         """
-        pass
+        while self.num_page >= 0:
+            if self.num_page % 5 == 0:
+                print(self.num_page)
+            self.load_page()
+            self.get_page_data()
 
 
     def create_dataframe(self):
         """
         Create the dataframe from all data collected
         """
-        pass
+        if len(self.data) == 0:
+            return
+        self.dataframe = pd.DataFrame(self.data, columns=['url', 'type', "date", "content", "file_info"])
 
 
     def save_dataframe_to_csv(self, path = "documents_enedis.csv"):
         """
         Save the documents in a csv
         """
-        pass
+        if self.dataframe is None:
+            return
+        self.dataframe.to_csv(path)
 
 
--- tests/test_scapping_docs_enedis.py
+++ tests/test_scapping_docs_enedis.py
@@ -1,35 +1,57 @@
 from scrapping.list_docs_enedis import ScrapListDocsEnedis
 import pytest
+import tempfile
+import pandas as pd
+import os
+
 
 @pytest.fixture()
-def resource():
-    print("setup")
-    yield "resource"
-    print("teardown")
+def scrap_with_page_loaded():
+    scrap = ScrapListDocsEnedis()
+    scrap.load_page()
+    yield scrap
 
-class TestScrapListDocsEnedis:
-    def test_that_depends_on_resource(self, resource):
-        print("testing {}".format(resource))
+
+@pytest.fixture()
+def scrap_all_done_no_df():
+    scrap = ScrapListDocsEnedis()
+    scrap.load_page()
+    scrap.get_page_data()
+    yield scrap
+
+@pytest.fixture()
+def scrap_all_done():
+    scrap = ScrapListDocsEnedis()
+    scrap.load_page()
+    scrap.get_page_data()
+    scrap.create_dataframe()
+    yield scrap
+
 
 def test_load_page():
-    assert True
+    scrap = ScrapListDocsEnedis()
+    scrap.load_page()
+    assert scrap.curent_page is not None
+    assert len(scrap.url_visited) > 0
 
 
-def test_get_page_data():
-    assert True
-
-
-def test_scrap_all():
-    assert True
-
-
-def test_scrap_all():
-    assert True
+def test_get_page_data(scrap_with_page_loaded):
+    scrap_with_page_loaded.get_page_data()
+    assert len(scrap_with_page_loaded.data) != 0
+    assert len(scrap_with_page_loaded.data[0]) == 5
 
  
-def test_create_dataframe():
-    assert True
+def test_create_dataframe(scrap_all_done_no_df):
+    scrap_all_done_no_df.create_dataframe()
+    assert scrap_all_done_no_df.dataframe is not None
+    assert type(scrap_all_done_no_df.dataframe) == pd.core.frame.DataFrame
 
 
-def test_save_dataframe_to_csv():
-    assert True
+def test_save_dataframe_to_csv(scrap_all_done):
+    test_dir = tempfile.TemporaryDirectory()
+    tmp_file_name = os.path.join(test_dir.name, f'documents_enedis.csv')
+    scrap_all_done.save_dataframe_to_csv(tmp_file_name)
+    assert os.path.isfile(tmp_file_name)
+    os.remove(tmp_file_name)
+
+

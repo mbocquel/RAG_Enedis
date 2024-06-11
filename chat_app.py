d9372dbc535d6056abc1bc6da9e04f8d8af6d1f2
Maxence Bocquel <maxence.bocquel@gmail.com>
2024-06-11 16:33:55 +0000

    App chat

--- chat_app.py
+++ chat_app.py
@@ -3,14 +3,14 @@ from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
-import os
 from dotenv import load_dotenv, find_dotenv
 
-_ = load_dotenv(find_dotenv())
+_ = load_dotenv(find_dotenv())  # Get the API key for Mistral
 
-@st.cache_resource(show_spinner="Creation de la connexion a Mistral")  
+
 def get_response(user_question, chat_history):
     """Function that call the LLM and get an answer"""
+
     template = """
         Tu es un super assistant. Repond aux questions de l'utilisateur en utilisant l'historique de conversation qui t'est donne. 
 
@@ -19,9 +19,9 @@ def get_response(user_question, chat_history):
         Question de l'utilisateur: {user_question}
     """
     prompt = ChatPromptTemplate.from_template(template)
-    llm = ChatMistralAI(mistral_api_key=os.environ['MISTRAL_API_KEY'], model_name="open-mistral-7b")
+    llm = ChatMistralAI(model_name="open-mistral-7b")
     chain = prompt | llm | StrOutputParser()
-    return chain.stream({"chat_history":chat_history, "user_question": user_question})
+    return chain.stream({"chat_history": chat_history, "user_question": user_question})
 
 
 # Initialise chat history
@@ -35,7 +35,9 @@ st.title("Enedis ChatBot")
 # Conversation
 for message in st.session_state.chat_history:
     if isinstance(message, HumanMessage):
-        with st.chat_message("Human", ):
+        with st.chat_message(
+            "Human",
+        ):
             st.markdown(message.content)
     else:
         with st.chat_message("AI"):
@@ -43,10 +45,12 @@ for message in st.session_state.chat_history:
 
 user_query = st.chat_input("Your message")
 if user_query is not None and user_query != "":
-    st.session_state.chat_history.append(HumanMessage(user_query))
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
     with st.chat_message("Human"):
         st.markdown(user_query)
 
     with st.chat_message("AI"):
-        ai_response = st.write_stream(get_response(user_query, st.session_state.chat_history))
-    st.session_state.chat_history.append(AIMessage(ai_response))
+        ai_response = st.write_stream(
+            get_response(user_query, st.session_state.chat_history)
+        )
+    st.session_state.chat_history.append(AIMessage(content=ai_response))
